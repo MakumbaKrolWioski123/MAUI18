b1ebbca6ff12a09bcd5b1b32a9dd1b7319453c91
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2026-01-29 10:24:50 +0100

    18.4 w budowie

--- Maui18/NewPage3.xaml.cs
+++ Maui18/NewPage3.xaml.cs
@@ -12,7 +12,9 @@ public partial class NewPage3 : ContentPage
 {
 	public NewPage3()
 	{
-		InitializeComponent();
+        BindingContext = new RachunekCalculatorVeiwModel();
+        InitializeComponent();
+
 	}
 
 	private async void LiczbaOsob(object sender, EventArgs e)
@@ -24,12 +26,60 @@ public partial class NewPage3 : ContentPage
 			OsobyEntry.IsVisible = true;
 			NaOsobeLabel.IsVisible = true;
 		}
-		else if (osoby == false) 
+		else if (osoby == false)
 		{
 			OsobyEntry.IsVisible = false;
 			NaOsobeLabel.IsVisible = false;
 		}
 	}
-	
-	
+
+	public class RachunekCalculatorVeiwModel : INotifyPropertyChanged
+	{
+        private double _kwota { get; set; }
+        private double _napiwek { get; set; }
+        private double _rachunek { get; }
+
+        public double Kwota
+        {
+            get => _kwota;
+            set
+            {
+                if (_kwota != value)
+                {
+                    _kwota = Math.Round(value, 2);
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(Rachunek));
+                }
+            }
+        }
+
+        public double Napiwek
+        {
+            get => _napiwek;
+            set
+            {
+                if (_napiwek != value)
+                {
+                    _napiwek = Math.Round(value / 100,2);
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(Rachunek));
+                }
+            }
+        }
+
+        public double Rachunek
+        {
+            get
+            {
+                if (_kwota <= 0) return 0;
+                return Math.Round(_kwota*_napiwek,2);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
